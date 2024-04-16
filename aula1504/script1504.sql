cc9efaa84fce32caa8a1b4e2aba0864cbf014da1
iingrud <ingridsantx@outlook.com>
2024-04-15 22:29:31 -0300

    Calculos de datas e subquery

---
+++ aula1504/script1504.sql
@@ -0,0 +1,12 @@
+/*Extração com SubQuery*/
+use empregados_db;
+select * from empregado where cod_depto in(
+    select cod_depto from departamento where cod_depto % 2 = 0
+);
+
+/*Calculo com datas*/
+select *, floor(datediff(now(), dt_nascimento) / 365) as idade from dependente;
+
+select * from empregado where cod_emp in(
+select cod_emp from dependente where floor(datediff(now(), dt_nascimento) / 365) < 18
+);
